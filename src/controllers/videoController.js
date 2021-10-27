a137285c39d4bdd39ac607c5f0796a050d6b8c83
yj <lollolllo328@gmail.com>
2021-10-27 22:47:25 +0900

    강의 #10까지

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,7 +3,7 @@ import User from "../models/User";
 
 
 export const home = async (req, res) => {
-    const videos = await Video.find({}).sort({createdAt: "desc"});
+    const videos = await Video.find({}).sort({createdAt: "desc"}).populate("owner");
     return res.render("home", {pageTitle: "Home", videos});
 };
 
@@ -94,7 +94,7 @@ export const search = async (req, res) => {
             title: {
                 $regex: new RegExp(keyword, "i")
             }
-        });
+        }).populate("owner");
     }
     return res.render("videos/search", {pageTitle: "Search", videos});
 }
